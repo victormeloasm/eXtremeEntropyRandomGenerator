6c12adc77e6bcef1d9c2d9ea47797e6dec9b95b8
Víctor Duarte Melo <victormeloasm@gmail.com>
2023-09-14 09:15:43 -0300

    Add files via upload

---
+++ eXtremeEntropyRandomGenerator.py
@@ -0,0 +1,64 @@
+import hashlib
+import pyaudio
+import cv2
+import numpy as np
+import random
+import os
+import wave
+import time
+
+while True:
+    try:
+        min_range = int(input("Enter the minimum value for random integers (0 or greater): "))
+        if min_range < 0:
+            raise ValueError("Minimum range must be 0 or greater")
+        break
+    except ValueError:
+        print("Invalid input. Please enter a valid minimum range.")
+
+while True:
+    try:
+        max_range = int(input("Enter the maximum value for random integers (up to 512-bit integer): "))
+        if max_range < 0:
+            raise ValueError("Maximum range must be greater than or equal to 0")
+        break
+    except ValueError as e:
+        print(e)
+
+audio = pyaudio.PyAudio()
+stream = audio.open(format=pyaudio.paInt16, channels=1, rate=44100, input=True, frames_per_buffer=44100)
+cap = cv2.VideoCapture(0)
+cv2.namedWindow("Webcam")
+cv2.namedWindow("Highlighted Image")
+
+random_bytes = os.urandom(1024)
+
+for i in range(10):
+    audio_data = stream.read(44100)
+    _, frame = cap.read()
+    timestamp = int(time.time())
+    random_integer = random.randint(min_range, max_range)
+    random_integer_bytes = random_integer.to_bytes(64, byteorder='big', signed=False)
+    combined_data = (
+            audio_data + frame.tobytes() + random_bytes +
+            timestamp.to_bytes(8, byteorder='big', signed=False) + random_integer_bytes
+    )
+    sha512 = hashlib.sha512()
+    sha512.update(combined_data)
+    hash_bytes = sha512.digest()
+    hash_integer = int.from_bytes(hash_bytes, byteorder="big")
+    result_integer = min_range + (hash_integer % (max_range - min_range + 1))
+    print("Calculated Integer within Range:", result_integer)
+
+    frame_copy = frame.copy()
+    cv2.putText(frame_copy, f"Value: {result_integer}", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+
+    cv2.setWindowProperty("Highlighted Image", cv2.WND_PROP_TOPMOST, 1)
+    cv2.imshow("Highlighted Image", frame_copy)
+    cv2.waitKey(1000)
+
+stream.stop_stream()
+stream.close()
+audio.terminate()
+cap.release()
+cv2.destroyAllWindows()
